ee61c2675a90cabc36fdf7786c2f4af9fd160fac
IrshamAja <m.irsham98@gmail.com>
2023-01-10 13:15:25 +0800

    rapikan route

--- routes/api.php
+++ routes/api.php
@@ -16,13 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
-Route::get('/posts/{id}', [PostController::class, 'show'])->middleware(['auth:sanctum']);
-
 Route::post('/login', [AuthenticationController::class, 'login']);
-Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware(['auth:sanctum']);
-Route::get('/me', [AuthenticationController::class, 'me'])->middleware(['auth:sanctum']);
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/logout', [AuthenticationController::class, 'logout']);
+    Route::get('/me', [AuthenticationController::class, 'me']);
+});
